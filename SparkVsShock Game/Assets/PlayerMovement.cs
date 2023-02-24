c91e43074b65d0a11691095f04b8986e8f1f8578
robertsa4611 <125394494+robertsa4611@users.noreply.github.com>
2023-02-24 13:07:57 -0600

    Adding Original Game FIles

---
+++ SparkVsShock Game/Assets/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private BoxCollider2D coll;
+
+    [SerializeField] private LayerMask jumpableGround;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //Change GetAxis to GetAxisRaw to have character stop immediately after pressing the move buttons
+        float moveX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveX * 7f, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+	    {
+		    rb.velocity = new Vector2(rb.velocity.x, 7f);
+	    }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    }
+
+}
