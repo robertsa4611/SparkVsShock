37869f1f7f4e4d84fe8ee37339809cdd6f8289ea
Nintendonerd64 <samthegamer1234@gmail.com>
2023-04-04 11:48:21 -0500

    Spike shit that isnt working yet
    I'm gonna try to get it working

---
+++ SparkVsShock Game/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int health;
+    public int maxHealth;
+    
+    
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health < 1)
+        {
+            Die();
+        }
+    }
+    
+    private void Die()
+    {
+        rb.bodyType = RigidbodyType2D.Static;
+    }
+}
